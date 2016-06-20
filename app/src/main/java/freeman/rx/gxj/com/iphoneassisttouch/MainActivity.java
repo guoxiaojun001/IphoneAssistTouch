084e19fa0082166ea1cb911f70e1e3f1a8f30b9b
kaku <guoxiaojun8804@126.com>
2016-06-20 18:34:37 +0800

    Android桌面悬浮窗效果实现，仿360手机卫士悬浮窗效果

---
+++ app/src/main/java/freeman/rx/gxj/com/iphoneassisttouch/MainActivity.java
@@ -0,0 +1,32 @@
+package freeman.rx.gxj.com.iphoneassisttouch;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import freeman.rx.gxj.com.iphoneassisttouch.service.FloatWindowService;
+
+
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startFloatWindow = (Button) findViewById(R.id.start_float_window);
+        startFloatWindow.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(MainActivity.this, FloatWindowService.class);
+                startService(intent);
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/freeman/rx/gxj/com/iphoneassisttouch/view/FloatWindowBigView.java
@@ -0,0 +1,53 @@
+package freeman.rx.gxj.com.iphoneassisttouch.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import freeman.rx.gxj.com.iphoneassisttouch.MyWindowManager;
+import freeman.rx.gxj.com.iphoneassisttouch.R;
+import freeman.rx.gxj.com.iphoneassisttouch.service.FloatWindowService;
+
+public class FloatWindowBigView extends LinearLayout {
+
+	/**
+	 * 记录大悬浮窗的宽度
+	 */
+	public static int viewWidth;
+
+	/**
+	 * 记录大悬浮窗的高度
+	 */
+	public static int viewHeight;
+
+	public FloatWindowBigView(final Context context) {
+		super(context);
+		LayoutInflater.from(context).inflate(R.layout.float_window_menu, this);
+		View view = findViewById(R.id.big_window_layout);
+		viewWidth = view.getLayoutParams().width;
+		viewHeight = view.getLayoutParams().height;
+		Button close = (Button) findViewById(R.id.close);
+		Button back = (Button) findViewById(R.id.back);
+		close.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 点击关闭悬浮窗的时候，移除所有悬浮窗，并停止Service
+				MyWindowManager.removeBigWindow(context);
+				MyWindowManager.removeSmallWindow(context);
+				Intent intent = new Intent(getContext(), FloatWindowService.class);
+				context.stopService(intent);
+			}
+		});
+		back.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// 点击返回的时候，移除大悬浮窗，创建小悬浮窗
+				MyWindowManager.removeBigWindow(context);
+				MyWindowManager.createSmallWindow(context);
+			}
+		});
+	}
+}
